71800296c047327170ff86cabefda9d2b2091a94
CVPR <74448065+cvpr-submission@users.noreply.github.com>
2022-11-16 22:07:43 +0800

    Add files via upload

---
+++ WinDB Generation/ERP2WinDB.py
@@ -0,0 +1,233 @@
+
+import os
+import cv2
+import Equirec2Perspec as E2P
+import numpy as np
+import copy
+import time
+from pandas import DataFrame
+
+## 将ERP转化为SPP
+## 这个是为了做消融实验
+
+color1=(1, 1, 1)
+color2=(0, 0, 0)
+
+# Read image
+num = 0
+## 投影平面大小
+width = 160
+height = 160
+## FOV的度数
+u_deg = 30
+v_deg = 30
+wFOV = 30
+hFOV = 30
+## 纱窗布局
+rows= 6
+columns = 12
+mydeg = 0
+equ_cx = (1920-1)/2 
+equ_cy = (960-1)/2
+
+def loadtxtmethod(filename):
+    data = np.loadtxt(filename,dtype=np.float,delimiter=',')
+    return data
+
+def addRoundedRectangleBorder(): 
+    cor = color2
+    img = np.ones((int(160*3/2), 160*4-30*2, 3), dtype=np.uint8)
+    height, width, channels = img.shape 
+    border_radius = 50 
+    line_thickness = 30
+    edge_shift = 0
+    cv2.line(img, (0, edge_shift), (width - 0, edge_shift), cor, line_thickness) 
+    cv2.line(img, (0, height), (width - 0, height), cor, line_thickness) 
+    cv2.line(img, (edge_shift, 0), (edge_shift, height - 0), cor, line_thickness) 
+    cv2.line(img, (width, 0), (width, height - 0), cor, line_thickness) 
+    #corners 
+    cv2.ellipse(img, (border_radius+ edge_shift, border_radius+edge_shift), (border_radius, border_radius), 180, 0, 90, cor, line_thickness) 
+    cv2.ellipse(img, (width-border_radius, border_radius), (border_radius, border_radius), 270, 0, 90, cor, line_thickness) 
+    cv2.ellipse(img, (width-border_radius, height-border_radius), (border_radius, border_radius), 0, 0, 90, cor, line_thickness) 
+    cv2.ellipse(img, (border_radius+edge_shift, height-border_radius),(border_radius, border_radius), 90, 0, 90, cor, line_thickness) 
+    return img
+
+def addRoundedRectangleBorderGray(): 
+    cor = color2
+    img = np.ones((int(160*3/2), 160*4, 3), dtype=np.uint8)
+    height, width, channels = img.shape 
+    border_radius = 40
+    line_thickness = 30
+    edge_shift = 0
+    cv2.line(img, (0, edge_shift), (width - 0, edge_shift), cor, line_thickness) 
+    cv2.line(img, (0, height), (width - 0, height), cor, line_thickness) 
+    cv2.line(img, (edge_shift, 0), (edge_shift, height - 0), cor, line_thickness) 
+    cv2.line(img, (width, 0), (width, height - 0), cor, line_thickness) 
+    #corners 
+    cv2.ellipse(img, (border_radius+ edge_shift, border_radius+edge_shift), (border_radius, border_radius), 180, 0, 90, cor, line_thickness) 
+    cv2.ellipse(img, (width-border_radius, border_radius), (border_radius, border_radius), 270, 0, 90, cor, line_thickness) 
+    cv2.ellipse(img, (width-border_radius, height-border_radius), (border_radius, border_radius), 0, 0, 90, cor, line_thickness) 
+    cv2.ellipse(img, (border_radius+edge_shift, height-border_radius),(border_radius, border_radius), 90, 0, 90, cor, line_thickness) 
+    return img
+
+## 渐变填充
+def smoothWD1(img): 
+    W = int(width/2)
+    M1 = img[:,0:W,:]
+    M2 = img[:,W:width,:]
+    kernels = [51,41,33,27,1]
+    mm = 0
+    resultA1 = np.empty((width,0,3),np.uint8)
+    for ii in range(0, W,  int(W/5)):
+        resultA1 = np.concatenate((resultA1,M1[:, ii:ii+int(W/5),:]),axis=1)
+        nn = kernels[mm]
+        mm = mm +1 
+        resultA2 = cv2.GaussianBlur(resultA1.astype('float64'), (nn, nn), nn, 0) # 填充
+        resultA1 = resultA2
+
+    kernels = [51,41,33,27,1]
+    mm = 0
+    resultA3 = np.empty((width,0,3),np.uint8)
+    for ii in range(W, 0, -int(W/5)):
+        resultA3 = np.concatenate((M2[:, ii-int(W/5):ii,:],resultA3),axis=1)
+        nn = kernels[mm]
+        mm = mm +1 
+        resultA4 = cv2.GaussianBlur(resultA3.astype('float64'), (nn, nn), nn, 0) # 填充
+        resultA3 = resultA4
+    return np.concatenate((resultA2,resultA4),axis=1)
+
+def smoothWD2(img): 
+    W = int(width/2)
+    M1 = img[:,0:W,:]
+    M2 = img[:,W:width,:]
+    kernels = [33,27,11,1,1]
+    mm = 0
+    resultA1 = np.empty((width,0,3),np.uint8)
+    for ii in range(0, W,  int(W/5)):
+        resultA1 = np.concatenate((resultA1,M1[:, ii:ii+int(W/5),:]),axis=1)
+        nn = kernels[mm]
+        mm = mm +1 
+        resultA2 = cv2.GaussianBlur(resultA1.astype('float64'), (nn, nn), nn, 0) # 填充
+        resultA1 = resultA2
+
+    kernels = [33,27,11,1,1]
+    mm = 0
+    resultA3 = np.empty((width,0,3),np.uint8)
+    for ii in range(W, 0, -int(W/5)):
+        resultA3 = np.concatenate((M2[:, ii-int(W/5):ii,:],resultA3),axis=1)
+        nn = kernels[mm]
+        mm = mm +1 
+        resultA4 = cv2.GaussianBlur(resultA3.astype('float64'), (nn, nn), nn, 0) # 填充
+        resultA3 = resultA4
+    return np.concatenate((resultA2,resultA4),axis=1)
+
+def addWindow():
+    mask2 = np.ones((960,1920,3), dtype=np.uint8)
+    cv2.line(mask2, (width*1,height*0),     (width*1,height*6),        color2, thickness=5)
+    cv2.line(mask2, (width*2,height*0),     (width*2,height*6),        color2, thickness=5)
+    cv2.line(mask2, (width*3,height*0),     (width*3,height*6),        color2, thickness=5)
+    cv2.line(mask2, (width*4,height*0),     (width*4,height*6),        color2, thickness=5)
+    cv2.line(mask2, (width*5,height*0),     (width*5,height*6),        color2, thickness=5)
+    cv2.line(mask2, (width*6,height*0),     (width*6,height*6),        color2, thickness=5)
+    cv2.line(mask2, (width*7,height*0),     (width*7,height*6),        color2, thickness=5)
+    cv2.line(mask2, (width*8,height*0),     (width*8,height*6),        color2, thickness=5)
+    cv2.line(mask2, (width*9,height*0),     (width*9,height*6),        color2, thickness=5)
+    cv2.line(mask2, (width*10,height*0),    (width*10,height*6),       color2, thickness=5)
+    cv2.line(mask2, (width*11,height*0),    (width*11,height*6),       color2, thickness=5)
+
+    cv2.line(mask2, (0,height*0),           (width*columns,height*0),  color2, thickness=10)
+    cv2.line(mask2, (0,height*1),           (width*columns,height*1),  color2, thickness=5)
+    cv2.line(mask2, (0,height*2),           (width*columns,height*2),  color2, thickness=5)
+    cv2.line(mask2, (0,height*3),           (width*columns,height*3),  color2, thickness=5)
+    cv2.line(mask2, (0,height*4),           (width*columns,height*4),  color2, thickness=5)
+    cv2.line(mask2, (0,height*5),           (width*columns,height*5),  color2, thickness=5)
+    cv2.line(mask2, (0,height*6),           (width*columns,height*6),  color2, thickness=10)
+    return mask2
+
+
+def equir2pers():
+    AwH = int(height*3/2)
+    AwW = width*4
+    #########################################################
+    MaskGray = np.ones((AwH, AwW*3, 3), dtype=np.uint8)
+    Mask = np.ones((AwH, AwW*3, 3), dtype=np.uint8)
+    Back = np.ones((1080, 1920, 3), dtype=np.uint8)*255
+    ######################################################
+
+    input_vid = './Video/'
+    output_dir = './Result/'
+    ori_name = os.listdir(input_vid)
+    ori_name.sort()
+    lon = loadtxtmethod('lon.txt') # 1440,1920
+    lat = loadtxtmethod('lat.txt')
+    lon = lon * equ_cx + equ_cx
+    lat = lat * equ_cy + equ_cy
+
+    window = addWindow()
+    Borderori = addRoundedRectangleBorder()
+    BorderGray = addRoundedRectangleBorderGray()
+
+    for file in range(0, len(ori_name)):
+        vid_name = os.path.join(input_vid, ori_name[file])
+
+        save_path = os.path.join(output_dir, ori_name[file][:-4])
+        if not os.path.exists(save_path):
+            os.makedirs(save_path)
+
+        cap =cv2.VideoCapture(vid_name)
+        num=0
+        while(cap.isOpened()):
+            ret, img = cap.read()
+            if ret==True:
+                # Read image
+                
+                if len(img.shape) == 2:
+                    img = img[..., None]
+                ori = cv2.resize(img,(1920,960),cv2.INTER_AREA)
+
+                # start_time = time.time()
+                persp = cv2.remap(ori, lon.astype(np.float32), lat.astype(np.float32), cv2.INTER_CUBIC, borderMode=cv2.BORDER_WRAP) 
+
+                resultBImg = persp[0:AwH,:,:]
+                resultAImg = persp[AwH:height*6+AwH,:,:]
+                resultCImg = persp[height*6+AwH:height*6+AwH+AwH,:,:]
+
+                for mm in range(0, rows):
+                    for nn in range(0, columns):  ## 第一行
+                        if mm == 0 or mm == (rows-1):
+                            Window = resultAImg[height*mm:height*(mm+1), width*nn:width*(nn+1),:]
+                            resultAImg[height*mm:height*(mm+1), width*nn:width*(nn+1),:] = smoothWD1(Window)
+                        if mm == 1 or mm == (rows-2):
+                            Window = resultAImg[height*mm:height*(mm+1), width*nn:width*(nn+1),:]
+                            resultAImg[height*mm:height*(mm+1), width*nn:width*(nn+1),:] = smoothWD2(Window)
+                resultAImg = resultAImg*window
+                for oo in range(0, 3):
+                    Border = np.concatenate((np.zeros((AwH, 30, 3),np.uint8),Borderori,np.zeros((AwH, 30, 3),np.uint8)),axis=1)
+                    resultBImg[:,AwW*oo:AwW*(oo+1),:] = resultBImg[:,AwW*oo:AwW*(oo+1),:]*Border
+                    MaskGray[:,AwW*oo:AwW*(oo+1),:] = MaskGray[:,AwW*oo:AwW*(oo+1),:]*BorderGray
+
+                    resultCImg[:,AwW*oo:AwW*(oo+1),:] = resultCImg[:,AwW*oo:AwW*(oo+1),:]*Border
+                    Mask[:,AwW*oo:AwW*(oo+1),:] = Mask[:,AwW*oo:AwW*(oo+1),:]* Border
+
+                resultAImg[:AwH,:,:] = resultAImg[:AwH,:,:]*MaskGray*0.5+resultAImg[:AwH,:,:]*(1-MaskGray)
+                resultAImg[-AwH:,:,:] = resultAImg[-AwH:,:,:]*MaskGray*0.5+resultAImg[-AwH:,:,:]*(1-MaskGray)
+                Back[60:1080-60,:,:] = resultAImg
+                Back[:AwH,:,:] = resultBImg*Mask + Back[:AwH,:,:]*(1-Mask)
+                Back[-AwH:,:,:] = resultCImg*Mask + Back[-AwH:,:,:]*(1-Mask)
+
+                '''end_time = time.time()
+                print("time cost:", float(end_time - start_time) * 1000.0, "ms")'''
+
+                cv2.imwrite(save_path+'\\'+vid_name[file][:-4]+'_%04d' % num+'.jpg', Back)  # 加浮窗
+                
+                
+                print('Finished')
+            num = num + 1
+            print(num)
+        else:
+            break
+        
+
+
+if __name__ == '__main__':
+    equir2pers()
---
+++ WinDB Generation/ERP2WinDBLonLat.py
@@ -0,0 +1,113 @@
+
+import os
+import cv2
+import Equirec2Perspec as E2P
+import numpy as np
+import copy
+import datetime
+from pandas import DataFrame
+
+def equir2windblonlat():
+    # Read image
+    num = 0
+    ## 投影平面大小
+    width = 160
+    height = 160
+    ## 纱窗布局
+    rows= 6
+    columns = 12
+    mydeg = 0
+    ######################################################
+    resultAImg = np.zeros((int(height*rows),int(width*columns), 3), dtype=np.float)
+    resultAlonlat = np.zeros((int(height*rows),int(width*columns), 2), dtype=np.float)
+    resultBImg = np.zeros((int(height*3/2),int(width*4)*3, 3), dtype=np.float)
+    resultBlonlat = np.zeros((int(height*3/2),int(width*4)*3, 2), dtype=np.float)
+    resultCImg = np.zeros((int(height*3/2),int(width*4)*3, 3), dtype=np.float)
+    resultClonlat = np.zeros((int(height*3/2),int(width*4)*3, 2), dtype=np.float)
+    ######################################################
+    time_now = datetime.datetime.now().strftime('%H:%M:%S.%f')
+    print(time_now)
+
+    ficpath = './Video/229.mp4'
+    num = 0
+
+    cap =cv2.VideoCapture(ficpath)
+
+    while(cap.isOpened()):
+        ret, img = cap.read()
+        if ret==True:
+            if len(img.shape) == 2:
+                img = img[..., None]
+            if num > 0:
+                break
+            num = num + 1
+            for mydeg in range(0, 1):
+                for nn in range(0, columns):  ## 第一行
+                    mu_deg = -165+nn*30
+                    equ = E2P.Equirectangular(img)    # Load equirectangular image
+                    Perimg, lonlat = equ.GetPerspective(30, 30, mu_deg, 75, height, width, mydeg)
+                    resultAlonlat[height*0:height*1, width*nn:width*(nn+1), :] = lonlat
+                    resultAImg[height*0:height*1, width*nn:width*(nn+1), :] = Perimg
+                for nn in range(0, columns):  ## 第二行
+                    mu_deg = -165+nn*30
+                    equ = E2P.Equirectangular(img)    # Load equirectangular image
+                    Perimg, lonlat = equ.GetPerspective(30, 30, mu_deg, 45, height, width, mydeg)
+                    resultAlonlat[height*1:height*2, width*nn:width*(nn+1), :] = lonlat
+                    resultAImg[height*1:height*2, width*nn:width*(nn+1), :] = Perimg
+                for nn in range(0, columns):  ## 第三行
+                    mu_deg = -165+nn*30
+                    equ = E2P.Equirectangular(img)    # Load equirectangular image
+                    Perimg, lonlat = equ.GetPerspective(30, 30, mu_deg, 15, height, width, mydeg)
+                    resultAlonlat[height*2:height*3, width*nn:width*(nn+1), :] = lonlat
+                    resultAImg[height*2:height*3, width*nn:width*(nn+1), :] = Perimg
+                for nn in range(0, columns):  ## 第四行
+                    mu_deg = -165+nn*30
+                    equ = E2P.Equirectangular(img)    # Load equirectangular image
+                    Perimg, lonlat = equ.GetPerspective(30, 30, mu_deg, -15, height, width, mydeg)
+                    resultAlonlat[height*3:height*4, width*nn:width*(nn+1), :] = lonlat
+                    resultAImg[height*3:height*4, width*nn:width*(nn+1), :] = Perimg
+                for nn in range(0, columns):  ## 第五行
+                    mu_deg = -165+nn*30
+                    equ = E2P.Equirectangular(img)    # Load equirectangular image
+                    Perimg, lonlat = equ.GetPerspective(30, 30, mu_deg, -45, height, width, mydeg)
+                    resultAlonlat[height*4:height*5, width*nn:width*(nn+1), :] = lonlat
+                    resultAImg[height*4:height*5, width*nn:width*(nn+1), :] = Perimg
+                for nn in range(0, columns):  ## 第六行
+                    mu_deg = -165+nn*30
+                    equ = E2P.Equirectangular(img)    # Load equirectangular image
+                    Perimg, lonlat = equ.GetPerspective(30, 30, mu_deg, -75, height, width, mydeg)
+                    resultAlonlat[height*5:height*6, width*nn:width*(nn+1), :] = lonlat
+                    resultAImg[height*5:height*6, width*nn:width*(nn+1), :] = Perimg
+
+                for nn in range(0, 3):  ## 第一行,辅屏
+                    mu_deg = -120+nn*120
+                    equ = E2P.Equirectangular(img)    # Load equirectangular image
+                    Perimg, lonlat = equ.GetPerspective(80, 50, mu_deg, 67, int(height*3/2), width*4, mydeg)
+                    resultBlonlat[0:int(height*3/2), width*4*nn:width*4*(nn+1), :] = lonlat[0:int(height*3/2),0:width*4,:]
+                    resultBImg[0:int(height*3/2), width*4*nn:width*4*(nn+1), :] = Perimg[0:int(height*3/2),0:width*4,:]
+                for nn in range(0, 3):  ## 第二行,辅屏
+                    mu_deg = -120+nn*120
+                    equ = E2P.Equirectangular(img)    # Load equirectangular image
+                    Perimg, lonlat = equ.GetPerspective(80, 50, mu_deg, -67, int(height*3/2), width*4, mydeg)
+                    resultClonlat[0:int(height*3/2), width*4*nn:width*4*(nn+1), :] = lonlat[0:int(height*3/2),0:width*4,:]
+                    resultCImg[0:int(height*3/2), width*4*nn:width*4*(nn+1), :] = Perimg[0:int(height*3/2),0:width*4,:]
+
+                resultImg = np.concatenate((resultBImg, resultAImg, resultCImg),axis=0)
+                resultlonlat = np.concatenate((resultBlonlat, resultAlonlat, resultClonlat),axis=0) # 240,1920,2; 960,1920,2; 240,1920,2->1440,1920
+
+                np.savetxt("./lon.txt", resultlonlat[:,:,0],fmt='%.4f',delimiter=',')
+                np.savetxt("./lat.txt", resultlonlat[:,:,1],fmt='%.4f',delimiter=',')
+                
+                print('Finished')
+            num = num + 1
+            print(num)
+            # print(num)
+            # if num > 1:
+            #   break
+        else:
+            break
+    
+
+
+if __name__ == '__main__':
+    equir2windblonlat()
