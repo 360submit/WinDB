8e39219ba4d1ccb2d081eb9918f73abe808e8210
CVPR <74448065+cvpr-submission@users.noreply.github.com>
2022-11-17 20:40:04 +0800

    Update ERP2WinDBLonLat.py

--- WinDB Generation/ERP2WinDBLonLat.py
+++ WinDB Generation/ERP2WinDBLonLat.py
@@ -8,12 +8,9 @@ import datetime
 from pandas import DataFrame
 
 def equir2windblonlat():
-    # Read image
     num = 0
-    ## 投影平面大小
     width = 160
     height = 160
-    ## 纱窗布局
     rows= 6
     columns = 12
     mydeg = 0
@@ -28,7 +25,7 @@ def equir2windblonlat():
     time_now = datetime.datetime.now().strftime('%H:%M:%S.%f')
     print(time_now)
 
-    ficpath = './Video/229.mp4'
+    ficpath = './Video/209.mp4'
     num = 0
 
     cap =cv2.VideoCapture(ficpath)
@@ -42,58 +39,58 @@ def equir2windblonlat():
                 break
             num = num + 1
             for mydeg in range(0, 1):
-                for nn in range(0, columns):  ## 第一行
+                for nn in range(0, columns):
                     mu_deg = -165+nn*30
-                    equ = E2P.Equirectangular(img)    # Load equirectangular image
+                    equ = E2P.Equirectangular(img)
                     Perimg, lonlat = equ.GetPerspective(30, 30, mu_deg, 75, height, width, mydeg)
                     resultAlonlat[height*0:height*1, width*nn:width*(nn+1), :] = lonlat
                     resultAImg[height*0:height*1, width*nn:width*(nn+1), :] = Perimg
-                for nn in range(0, columns):  ## 第二行
+                for nn in range(0, columns):
                     mu_deg = -165+nn*30
-                    equ = E2P.Equirectangular(img)    # Load equirectangular image
+                    equ = E2P.Equirectangular(img)
                     Perimg, lonlat = equ.GetPerspective(30, 30, mu_deg, 45, height, width, mydeg)
                     resultAlonlat[height*1:height*2, width*nn:width*(nn+1), :] = lonlat
                     resultAImg[height*1:height*2, width*nn:width*(nn+1), :] = Perimg
-                for nn in range(0, columns):  ## 第三行
+                for nn in range(0, columns):
                     mu_deg = -165+nn*30
-                    equ = E2P.Equirectangular(img)    # Load equirectangular image
+                    equ = E2P.Equirectangular(img)
                     Perimg, lonlat = equ.GetPerspective(30, 30, mu_deg, 15, height, width, mydeg)
                     resultAlonlat[height*2:height*3, width*nn:width*(nn+1), :] = lonlat
                     resultAImg[height*2:height*3, width*nn:width*(nn+1), :] = Perimg
-                for nn in range(0, columns):  ## 第四行
+                for nn in range(0, columns):
                     mu_deg = -165+nn*30
-                    equ = E2P.Equirectangular(img)    # Load equirectangular image
+                    equ = E2P.Equirectangular(img)
                     Perimg, lonlat = equ.GetPerspective(30, 30, mu_deg, -15, height, width, mydeg)
                     resultAlonlat[height*3:height*4, width*nn:width*(nn+1), :] = lonlat
                     resultAImg[height*3:height*4, width*nn:width*(nn+1), :] = Perimg
-                for nn in range(0, columns):  ## 第五行
+                for nn in range(0, columns):
                     mu_deg = -165+nn*30
-                    equ = E2P.Equirectangular(img)    # Load equirectangular image
+                    equ = E2P.Equirectangular(img)
                     Perimg, lonlat = equ.GetPerspective(30, 30, mu_deg, -45, height, width, mydeg)
                     resultAlonlat[height*4:height*5, width*nn:width*(nn+1), :] = lonlat
                     resultAImg[height*4:height*5, width*nn:width*(nn+1), :] = Perimg
-                for nn in range(0, columns):  ## 第六行
+                for nn in range(0, columns):
                     mu_deg = -165+nn*30
-                    equ = E2P.Equirectangular(img)    # Load equirectangular image
+                    equ = E2P.Equirectangular(img)
                     Perimg, lonlat = equ.GetPerspective(30, 30, mu_deg, -75, height, width, mydeg)
                     resultAlonlat[height*5:height*6, width*nn:width*(nn+1), :] = lonlat
                     resultAImg[height*5:height*6, width*nn:width*(nn+1), :] = Perimg
 
-                for nn in range(0, 3):  ## 第一行,辅屏
+                for nn in range(0, 3):
                     mu_deg = -120+nn*120
-                    equ = E2P.Equirectangular(img)    # Load equirectangular image
-                    Perimg, lonlat = equ.GetPerspective(80, 50, mu_deg, 67, int(height*3/2), width*4, mydeg)
+                    equ = E2P.Equirectangular(img)
+                    Perimg, lonlat = equ.GetPerspective(80, 48, mu_deg, 68, int(height*3/2), width*4, mydeg)
                     resultBlonlat[0:int(height*3/2), width*4*nn:width*4*(nn+1), :] = lonlat[0:int(height*3/2),0:width*4,:]
                     resultBImg[0:int(height*3/2), width*4*nn:width*4*(nn+1), :] = Perimg[0:int(height*3/2),0:width*4,:]
-                for nn in range(0, 3):  ## 第二行,辅屏
+                for nn in range(0, 3):
                     mu_deg = -120+nn*120
-                    equ = E2P.Equirectangular(img)    # Load equirectangular image
-                    Perimg, lonlat = equ.GetPerspective(80, 50, mu_deg, -67, int(height*3/2), width*4, mydeg)
+                    equ = E2P.Equirectangular(img)
+                    Perimg, lonlat = equ.GetPerspective(80, 48, mu_deg, -68, int(height*3/2), width*4, mydeg)
                     resultClonlat[0:int(height*3/2), width*4*nn:width*4*(nn+1), :] = lonlat[0:int(height*3/2),0:width*4,:]
                     resultCImg[0:int(height*3/2), width*4*nn:width*4*(nn+1), :] = Perimg[0:int(height*3/2),0:width*4,:]
 
                 resultImg = np.concatenate((resultBImg, resultAImg, resultCImg),axis=0)
-                resultlonlat = np.concatenate((resultBlonlat, resultAlonlat, resultClonlat),axis=0) # 240,1920,2; 960,1920,2; 240,1920,2->1440,1920
+                resultlonlat = np.concatenate((resultBlonlat, resultAlonlat, resultClonlat),axis=0)
 
                 np.savetxt("./lon.txt", resultlonlat[:,:,0],fmt='%.4f',delimiter=',')
                 np.savetxt("./lat.txt", resultlonlat[:,:,1],fmt='%.4f',delimiter=',')
@@ -101,9 +98,6 @@ def equir2windblonlat():
                 print('Finished')
             num = num + 1
             print(num)
-            # print(num)
-            # if num > 1:
-            #   break
         else:
             break
     
